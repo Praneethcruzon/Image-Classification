5a34ea7e6ed6f34d1bf6d6d8af97b11b5dd6f993
Praneeth <praneethcruzon@gmail.com>
2022-04-30 10:57:37 +0530

    implemented data loader, loss function and optimizer

--- train.py
+++ train.py
@@ -1,3 +1,67 @@
+
+# Ref https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
+import torch
+
+from torch.utils.data import DataLoader
+# DataLoader wraps a iteratable object around Dataset to enable easy access to samples
+from torch.utils.data import random_split
+# To split the dataset into train and val splits. Randomly split a dataset into non-overlapping new datasets of given lengths.
+from torchvision import transforms
+from load_dataset import SportsDataset
+from cnn import CNN
+
+DATASET_PATH = "dataset"
+CSV_PATH = "train_labels.csv"
+ROOT_DIR = "./"
+ID = 20
+
+# Load Dataset into torch.utils.data.Dataset class
+sports_dataset = SportsDataset(
+    csv_path = CSV_PATH, 
+    root_dir = ROOT_DIR, 
+    dataset_dir = DATASET_PATH,
+    # Ref https://pytorch.org/vision/0.9/transforms.html#torchvision.transforms.Resize
+    transform = transforms.Compose([
+        transforms.Resize(size = (300, 300), interpolation = transforms.InterpolationMode.BILINEAR)
+    ])
+    )
+
+
+print(f"Dataset Length : {sports_dataset.__len__()}")
+
+# Printing sample data
+image, label = sports_dataset.__getitem__(ID)
+print(f"Random Sample ID = {ID} : {image.shape} , {label}")
+
+# Training and validation split. 
+# ref : https://pytorch.org/docs/stable/data.html#torch.utils.data.random_split
+train_dataset, val_dataset = random_split(
+    sports_dataset,  # The dataset that needs to be split
+    [int(0.8 * sports_dataset.__len__()), int(0.2 * sports_dataset.__len__())], # Length of the resultant splits
+    generator = torch.Generator().manual_seed(10) # A generator object to feed in random numbers.
+    )
+
+print(f"No of images in Training Set = {train_dataset.__len__()}")
+print(f"No of images in Validation Set = {val_dataset.__len__()}")
+
+# Training and validation dataloader from torch.utils.data.DataLoader
+train_dataloader = DataLoader(train_dataset, batch_size = 16, shuffle = True)
+val_dataloader = DataLoader(val_dataset, batch_size = 16, shuffle = True)
+
+
+# LOADING MODEL FROM cnn.py
+# ref : https://pytorch.org/tutorials/beginner/introyt/trainingyt.html#loss-function
+model = CNN()
+
+# LOSS FUNCTION
+loss_fn = torch.nn.CrossEntropyLoss()
+
+# OTIMIZER 
+# ref : https://pytorch.org/tutorials/beginner/introyt/trainingyt.html#optimizer
+optimizer = torch.otim.SGD(model.parameters(), lr = 0.001, momentum = 0.9)
+
+# TRAINING LOOP
+# -------------
 # Gets a batch of training data from the DataLoader
 # Zeros the optimizer’s gradients
 # Performs an inference - that is, gets predictions from the model for an input batch
@@ -6,61 +70,5 @@
 # Tells the optimizer to perform one learning step - that is, adjust the model’s learning weights based on the observed gradients for this batch, according to the optimization algorithm we chose
 # It reports on the loss for every 1000 batches.
 # Finally, it reports the average per-batch loss for the last 1000 batches, for comparison with a validation run
+# Ref : https://pytorch.org/tutorials/beginner/introyt/trainingyt.html#the-training-loop
 
-# Ref https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
-# Ref - Custom Dataloader - https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-custom-dataset-for-your-files
-
-from torch.utils.data import Dataset
-# Dataset stores samples and their corresponding labels
-# DataLoader wraps a iteratable object around Dataset to enable easy access to samples
-import pandas as pd
-# from PIL import Image    # Could use torchvision.io.read_image instead. 
-# Converts an image into a tensor rather than a numpy array in opencv. 
-from torchvision.io import read_image
-import os
-
-
-class SportsDataset(Dataset):
-    # Dataset from torch.utils.data is a abstract class that we need to derive and use
-    # to represent our dataset with. we will have to override __init__, __len__ and __getitem__ functions.
-    def __init__(self, csv_path, root_dir, dataset_dir, transform = None, target_transform = None):
-
-        self.sports_data = pd.read_csv(os.path.join(dataset_dir, csv_path))
-        self.root_dir = root_dir
-        self.dataset_dir = dataset_dir
-        # Transforms applied to image
-        self.transform = transform   
-        # Transforms applied to label. 
-        self.target_transform = target_transform
-    
-    def __len__(self):
-        return len(self.sports_data)
-
-    def __getitem__(self, id):
-        record = self.sports_data.iloc[id]
-       
-        image = read_image(os.path.join(self.dataset_dir, os.path.dirname(record['image'][2:]), os.path.basename(record['image'])))
-
-        if self.transform:
-            image = self.transform(image)
-
-        label = record['sports']
-        if self.target_transform:
-            label = self.target_transform(label)
-
-        return image, label
-
-
-DATASET_PATH = "dataset"
-CSV_PATH = "train_labels.csv"
-ROOT_DIR = "./"
-ID = 10
-
-sports_dataset = SportsDataset(CSV_PATH, ROOT_DIR, DATASET_PATH)
-
-print(f"Dataset Length : {sports_dataset.__len__()}")
-
-print(f"ID {ID} : \n{sports_dataset.__getitem__(ID)}")
-
-
-# Should work on preparing dataset for training with DataLoaders. Ref: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-custom-dataset-for-your-files
